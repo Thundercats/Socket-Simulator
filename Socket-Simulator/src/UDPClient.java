eb7fa6fcb38b66f31d92ee9d3a7780f16c337500
Sri <srisreed@gmail.com>
2012-05-02 01:00:15 -0700

    Modified for readability, still need to improve

--- Socket-Simulator/src/UDPClient.java
+++ Socket-Simulator/src/UDPClient.java
@@ -4,28 +4,63 @@ import java.net.*;
 
 public class UDPClient {
    
+    private static final int PORT_NUM = 9999;
+    private static DatagramSocket socket;
+    private static DatagramPacket packet;
+    private static byte[] bytesSent;
+    private static final int MAX = 1000;
+    private static final int DATA_SIZE = 1024;
+    private static InetAddress address;
+    private String receivedMessage;
+    
+    UDPClient() throws SocketException, UnknownHostException {
+        
+        socket = new DatagramSocket();
+        bytesSent = new byte[DATA_SIZE];
+        //address = InetAddress.getByName("localhost");
+        //packet = new DatagramPacket(bytesSent, bytesSent.length, address, PORT_NUM);
+    }
+    
+    
+    public void send(DatagramPacket aPacket) throws IOException
+    {
+        socket.send(aPacket);
+    }
+    
+    public void setAddress(String name) throws UnknownHostException
+    {
+        address = InetAddress.getByName(name);
+    }
+    
+    public void createPacket(byte[] aBytesSent, int len, InetAddress ipAddress)
+    {
+        packet = new DatagramPacket(aBytesSent, len, ipAddress, PORT_NUM);
+    }
+    
+    public void setMessage(String message)
+    {
+        bytesSent = message.getBytes();
+    }
+    
+    public void receiveMessage(byte[] aByte)
+    {
+        receivedMessage = new String(aByte);
+        System.out.println("Received message: " + receivedMessage);
+    }
+    
+    public void close()
+    {
+        socket.close();
+    }
+    
     public static void main(String[] args) throws IOException {
         
-        DatagramSocket socket = new DatagramSocket();
-        
-        byte[] bytesSent = new byte[1024];
-        
-        String message = "hello, this is the client";
-        bytesSent = message.getBytes();
-        
-        //address of machine
-        InetAddress address = InetAddress.getByName("localhost");
-        int port = 4445;
-        
-        //packet to send
-        DatagramPacket packet = new DatagramPacket(bytesSent, bytesSent.length, address, port);
-        socket.send(packet);
-        
-        //received packet
-        String receivedMessage = new String(packet.getData());
-        System.out.println("Received message: " + receivedMessage);
-        
-        socket.close();
+        UDPClient client = new UDPClient();
+        client.setMessage("hello, this is the client");
+        client.setAddress("localhost");
+        client.createPacket(bytesSent, bytesSent.length, address);
+        client.send(packet);
+        client.receiveMessage(packet.getData());
     }
     
 }
