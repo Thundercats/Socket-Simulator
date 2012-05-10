476b0f0691f5ff5a55fa82ca6ee2fc5a8eacb2eb
Sri <srisreed@gmail.com>
2012-05-09 18:24:00 -0700

    Added different message sizes, loop each 100 times and calculates the Avg RTT for each. Note: we need to format the output properly.

--- Socket-Simulator/src/TCPServer.java
+++ Socket-Simulator/src/TCPServer.java
@@ -2,7 +2,13 @@ import java.net.*;
 import java.io.*;
  
 public class TCPServer {
-    
+    private static final int ONE_KB = 1024;
+    private static final int TWO_KB = 2048;
+    private static final int FOUR_KB = 4096;
+    private static final int EIGHT_KB = 8192;
+    private static final int SIXTEEN_KB = 16384;
+    private static final int THIRTY_TWO_KB = 32768;
+    private static final int SIXTY_FOUR_KB = 65536;
     //private ServerSocket serverSocket;
 //    
 //    public TCPServer(int portNum) throws IOException
@@ -37,7 +43,7 @@ public class TCPServer {
     {
        int port = 9999;
        String data = "L";
-       byte geoff = 'L'; 
+       byte geoff = 'L';
        try
             {
             ServerSocket serverSocket = new ServerSocket(port);
@@ -45,18 +51,29 @@ public class TCPServer {
             
             System.out.println("Connection Established");
             PrintWriter out = new PrintWriter(skt.getOutputStream(), true);
-            System.out.println("Data sending: '" + data + "'\n");
+            System.out.println("Data sending: '" + ONE_KB + "'\n");
             
-            long start =  System.nanoTime();
+            int messageSize = ONE_KB;
+            //for(int i = 0; i < 1000; i++)
+            byte[] bytes;
             
-            for(int i = 0; i < 1000; i++)
-            {
-                out.print(geoff);
+            while (messageSize <= SIXTY_FOUR_KB) {
+              
+                long start =  System.nanoTime();
+                bytes = new byte[messageSize];
+                
+                for (int i = 0; i < 100; i++) {
+                        out.print(ONE_KB);
+                    }
+
+                    
+                    
+                long diff = System.nanoTime() - start;
+                double timeInSeconds = diff / 1000000000.0; 
+                out.print("The avg RTT is " + timeInSeconds / 1000);
+                messageSize *= 2;
             }
             
-            long diff = System.nanoTime() - start;
-            double timeInSeconds = diff / 1000000000.0; 
-            out.print("The avg RTT is " + timeInSeconds / 1000);
             out.close();
             skt.close();
             serverSocket.close();
