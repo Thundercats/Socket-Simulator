f49c25ca570b95ae9cdd780e68e0012c37372a5d
kamron <adrian@adrianblog.net>
2012-05-13 22:10:32 -0700

    Most up to date code, via Sri.

--- Socket-Simulator/src/TCPClient.java
+++ Socket-Simulator/src/TCPClient.java
@@ -2,33 +2,98 @@ import java.io.*;
 import java.net.*;
  
 public class TCPClient {
+    private static DataOutputStream out;
+    private static DataInputStream in;
+    private static Socket socket;
+    
+    private static final int ONE_KB = 1024; 
+    private static final int SIXTY_FOUR_KB = 65536;
+    private static byte[] bytes;
+    private static byte[] inBytes;
     
     public static void main(String[] args)
     {
-	String host = "localhost";
-	if(args.length>0)
-	    host=args[0];
         try {
-            Socket socket = new Socket(host, 9999); //socket
-            int i = 0;
-            System.out.println("Connection established");
-            
-            DataInputStream in =
-                    new DataInputStream(socket.getInputStream());
-            
-            System.out.println(i + " from server: " + in.readLine());
-            
-            //DataOutputStream out = 
-            //        new DataOutputStream(socket.getOutputStream());//out
-            //BufferedReader in = 
-                    //new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             
+             socket = new Socket("localhost", 9999); //socket 
+             
+             System.out.println("Connection established");
+
+             out = new DataOutputStream(socket.getOutputStream()); //Data to send OUT to Server
+             System.out.println("out created");
+             in = new DataInputStream(socket.getInputStream()); //Data being read IN from server
+             System.out.println("in created");
+             int messageSize = ONE_KB;  
+             
+             byte sendByte = "a".getBytes()[0];
+             byte receivedByte;
+             
+             long start = System.nanoTime(); 
+             for(int i = 0; i < 1000; i++)
+             {
+                 out.write(sendByte);
+                 out.flush();
+                 receivedByte = in.readByte();
+             }
+             
+             long diff = System.nanoTime() - start;
+             double timeInSeconds = diff / 1000000000.0;
+             
+             System.out.println("The size is " + " 1 byte " + " The avg RTT is " + timeInSeconds / 1000);
+             
+             /*
+              * I commented out the while loop, currently only sending 1 KB to server
+              */
+             start = 0;
+             diff = 0;
+             timeInSeconds = 0;
+             
+             while (messageSize <= SIXTY_FOUR_KB) {
+             //
+                
+                bytes = new byte[messageSize];
+                inBytes = new byte[messageSize];
+                //System.out.println("arrays created");
+                
+                
+                /*
+                 * filling the bytes array with "a"
+                 */
+                for(int i = 0; i < messageSize; i++)
+                {
+                     //System.out.println("byte array beign filled");
+                    bytes[i] = "a".getBytes()[0]; 
+                }
+                
+                start = System.nanoTime(); 
+                for (int i = 0; i < 100; i++) 
+                {   
+                        out.write(bytes, 0, bytes.length); 
+                        //out.writeByte(i);
+                        //System.out.println("i " + i + " data sending out >>>>");
+                        out.flush(); 
+                        in.readFully(inBytes, 0, inBytes.length);
+                        
+                        //System.out.println("i " + i + " data receiving in <<<<<<");
+                }
+                  
+                diff = System.nanoTime() - start;
+                timeInSeconds = diff / 1000000000.0;
+                
+                //out.print("The avg RTT is " + timeInSeconds / 1000);
+                System.out.println("The size is " + messageSize + " The avg RTT is " + timeInSeconds / 100);
+                //System.out.println("message size before: " + messageSize);
+                messageSize *= 2;
+                //System.out.println("message size after: " + messageSize);
+            } 
             
             in.close();
-            //socket.close();
+            out.close();
+            socket.close();
         }
         catch(Exception e)
         {
-            System.out.println("We gots an error "+ e.toString());
+            System.out.println("Error, please try again "+ e.toString());
         }
     }
 }
