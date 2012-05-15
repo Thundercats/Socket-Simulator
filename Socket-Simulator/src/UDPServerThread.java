406ac8efc6dab2eaf283ba86991305f8aa2825ab
Sri <srisreed@gmail.com>
2012-05-15 13:42:34 -0700

    Updated the way it sends data back.

--- Socket-Simulator/src/UDPServerThread.java
+++ Socket-Simulator/src/UDPServerThread.java
@@ -16,8 +16,9 @@ public class UDPServerThread extends Thread {
     private static byte[] bytesReceived;
     private static byte[] bytesSend;
     private static String message;
-    private static int DATA_SIZE = 65536; //64 KB
+    private static int DATA_SIZE = 1024; //64 KB65536
     private static int port;
+    private static int length;
     
     public UDPServerThread() throws IOException {
         this("UPDServerThread");
@@ -43,16 +44,19 @@ public class UDPServerThread extends Thread {
                 bytesReceived = new byte[DATA_SIZE];
                 packet = new DatagramPacket(bytesReceived, bytesReceived.length);
                 socket.receive(packet);
-                message = new String(packet.getData(),0,packet.getLength());
+                //message = new String(packet.getData(),0,packet.getLength());
                 
-                bytesSend = new byte[DATA_SIZE];
-                bytesSend = ("Server received " + message).getBytes();
+                //bytesSend = new byte[DATA_SIZE];
+                //bytesSend = ("Server received " + message).getBytes();
                 
-                System.out.println("this is it: " + new String(bytesSend));
+                //System.out.println("this is it: " + new String(bytesSend));
                 
                 address = packet.getAddress();
                 port = packet.getPort();
-                packet2 = new DatagramPacket(bytesSend, bytesSend.length, address, port);
+                //bytesSend = packet.getData();
+                //length = packet.getData().length;
+                //packet2 = new DatagramPacket(bytesSend, bytesSend.length, address, port);
+                packet2 = new DatagramPacket(packet.getData(),packet.getData().length,address, port);
                 socket.send(packet2);
                 
                 System.out.println("Message received: " + message);
