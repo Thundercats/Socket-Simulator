13251fcc63944e385dde91a0489c81dc649a591e
Sri <srisreed@gmail.com>
2012-05-15 15:53:12 -0700

    Added flags to try to change buffer size dynamically. (not worrking yet)

--- Socket-Simulator/src/TCPClient.java
+++ Socket-Simulator/src/TCPClient.java
@@ -6,7 +6,12 @@ public class TCPClient {
     private static DataInputStream in;
     private static Socket socket;
     
-    private static final int ONE_KB = 1024; 
+    private static final int ONE_KB = 1024;
+    private static final int TWO_KB = 2048;
+    private static final int FOUR_KB = 4096;
+    private static final int EIGHT_KB = 8192;
+    private static final int SIXTEEN_KB = 16384;
+    private static final int THIRTY_TWO_KB = 32768;
     private static final int SIXTY_FOUR_KB = 65536;
     private static byte[] bytes;
     private static byte[] inBytes;
@@ -14,7 +19,6 @@ public class TCPClient {
     public static void main(String[] args)
     {
         try {
-             
              socket = new Socket("localhost", 9999); //socket 
              
              System.out.println("Connection established");
@@ -23,38 +27,37 @@ public class TCPClient {
              System.out.println("out created");
              in = new DataInputStream(socket.getInputStream()); //Data being read IN from server
              System.out.println("in created");
-             int messageSize = ONE_KB;  
              
              //1 BYTE ONLY
              byte sendByte = "a".getBytes()[0];
-             byte receivedByte;
-             
-             long start = System.nanoTime();  
+             long diff = 0;
+             long start = 0;  
+             out.write("A".getBytes()[0]); //flag
              for(int i = 0; i < 1000; i++)
              {
+                 start = System.nanoTime(); 
                  out.write(sendByte);
                  out.flush();
-                 receivedByte = in.readByte();
+                 in.readByte(); //received byte
+                 diff += System.nanoTime() - start;
              }
              
-             long diff = System.nanoTime() - start;
+              
              double timeInSeconds = diff / 1000000000.0;
              
              System.out.println("The size is " + " 1 byte " + " The avg RTT is " + timeInSeconds / 1000);
-             
              
              //Reset and Send 1KB - 64KB
              start = 0;
              diff = 0;
              timeInSeconds = 0;
+             int messageSize = ONE_KB; 
              
              while (messageSize <= SIXTY_FOUR_KB) {
-             //
-                
+             
                 bytes = new byte[messageSize];
                 inBytes = new byte[messageSize];
                 //System.out.println("arrays created");
-                
                 
                 /*
                  * filling the bytes array with "a"
@@ -65,24 +68,61 @@ public class TCPClient {
                     bytes[i] = "a".getBytes()[0]; 
                 }
                 
-                start = System.nanoTime(); 
-                byte totalData = 0;
+                
+                //byte totalData = 0;
+                int totalData = 0;
+                
+                //flags for letting server know what size to make buffer
+                if(messageSize == ONE_KB)
+                {
+                   out.write("b".getBytes()[0]); 
+                }
+                else if(messageSize == TWO_KB)
+                {
+                   out.write("c".getBytes()[0]);
+                }
+                else if(messageSize == FOUR_KB)
+                {
+                    out.write("d".getBytes()[0]);
+                }
+                else if(messageSize == EIGHT_KB)
+                {
+                    out.write("e".getBytes()[0]);
+                }
+                else if(messageSize == SIXTEEN_KB)
+                {
+                    out.write("f".getBytes()[0]);
+                }
+                else if(messageSize == THIRTY_TWO_KB)
+                {
+                    out.write("g".getBytes()[0]);
+                }
+                else
+                {
+                    out.write("h".getBytes()[0]);
+                }
+                
+                out.flush();
+                
                 for (int i = 0; i < 100; i++) 
-                {   
-                        out.write(bytes, 0, bytes.length); 
-                        //out.writeByte(i);
-                        //System.out.println("i " + i + " data sending out >>>>");
-                        out.flush(); 
-                        in.readFully(inBytes, 0, inBytes.length); 
-                        totalData += inBytes[i];
-                        //System.out.println("i " + i + " data receiving in <<<<<<");
+                { 
+                    start = System.nanoTime();     
+                    out.write(bytes, 0, bytes.length); 
+                    //out.writeByte(i);
+                    //System.out.println("i " + i + " data sending out >>>>");
+                    out.flush(); 
+                    in.readFully(inBytes, 0, inBytes.length); 
+                    totalData += inBytes[i];
+                    diff += System.nanoTime() - start;
+                    //System.out.println("i " + i + " data receiving in <<<<<<");
                 }
                   
-                diff = System.nanoTime() - start;
+                
                 timeInSeconds = diff / 1000000000.0;
                 double throughput = (totalData * 8) / timeInSeconds;
+                //double throughput = (messageSize * 100) / timeInSeconds;
                 //out.print("The avg RTT is " + timeInSeconds / 1000);
-                System.out.println("The size is " + messageSize + " The avg RTT is " + timeInSeconds / 100 + " throughput: " + throughput); 
+                System.out.println( messageSize + "   "  + timeInSeconds / 100 + "   " + throughput + "\n"); 
                 //System.out.println("message size before: " + messageSize);
                 messageSize *= 2;
                 //System.out.println("message size after: " + messageSize);
--- Socket-Simulator/src/TCPServer.java
+++ Socket-Simulator/src/TCPServer.java
@@ -14,6 +14,7 @@ public class TCPServer {
     private static DataOutputStream out;
     private static ServerSocket serverSocket;
     private static Socket skt;
+    private static int bufferSize;
  
     public static void main(String[] args) throws IOException
     {
@@ -24,14 +25,52 @@ public class TCPServer {
                 skt = serverSocket.accept();
                 in = new DataInputStream(skt.getInputStream());
                 out = new DataOutputStream(skt.getOutputStream());
+                
                 bytes = new byte[ONE_KB];
+                //bytes = new byte[SIXTY_FOUR_KB];
                 System.out.println("Connection Established"); 
                 
+                byte flag;
+                boolean safe = true;
+                byte a = "A".getBytes()[0];
+                byte one = "b".getBytes()[0];
+                byte two = "c".getBytes()[0];
+                byte four = "d".getBytes()[0];
+                byte eight = "e".getBytes()[0];
+                byte sixteen = "f".getBytes()[0];
+                byte thirty_two = "g".getBytes()[0]; 
+                
                 while(true) 
-                {
-                  in.read(bytes);
+                { 
+                   if(safe == true)
+                   {
+                       flag = in.readByte();
+
+                       if (flag == a) {
+                           bytes = new byte[1];
+                       } else if (flag == one) {
+                           bytes = new byte[ONE_KB];
+                       } else if (flag == two) {
+                           bytes = new byte[TWO_KB];
+                       } else if (flag == four) {
+                           bytes = new byte[FOUR_KB];
+                       } else if (flag == eight) {
+                           bytes = new byte[EIGHT_KB];
+                       } else if (flag == sixteen) {
+                           bytes = new byte[SIXTEEN_KB];
+                       } else if (flag == thirty_two) {
+                           bytes = new byte[THIRTY_TWO_KB];
+                       } else {
+                           bytes = new byte[SIXTY_FOUR_KB];
+                       }
+                       
+                        safe = false;
+                   }
+                  
+                    
+                  in.readFully(bytes);
                   System.out.println("reading in");
-                  out.write(bytes);   
+                  out.write(bytes, 0 , bytes.length);   
                   System.out.println("sending out");
                 }
                  
